b46b3876ab709a591f154ff4fb5681f470bff5e0
multapaucis <matthewrfree@gmail.com>
2018-02-13 15:50:51 -0800

    Add Create and Read DB functionality to WebServer2

--- vagrant/menus/webserver2.py
+++ vagrant/menus/webserver2.py
@@ -1,36 +1,84 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base
+
+# Create a DB Session
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
+            if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+
+                restaurants = session.query(Restaurant).all()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                for rest in restaurants:
+                    output += "<h2> %s </h2>" % rest.name
+                    output += "<a href='/edit'>Edit</a><br>"
+                    output += "<a href='/edit'>Delete</a><br>"
+
+                output += "<br><h2><a href='/restaurants/new'>Add a New Restaurant</a></h2>"
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
 
-            if self.path.endswith("/hola"):
+            elif self.path.endswith("/restaurants/new"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+
                 output = ""
                 output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><h2>What is the new restaurant's Name?</h2><input name='rname' type='text' ><input type='submit' value='Submit'> </form>"
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
+
+            elif self.path.endswith("/edit"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                output += "<html><body>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/edit'><h2>What is the restaurant's New Name?</h2><input name='uname' type='text' ><input type='submit' value='Submit'> </form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
+            else:
+                self.send_error(404, "I can't find %s. try harder" % self.path)
+
+                '''
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                output += "<html><body>"
+                for rest in restaurants:
+                    output += "<h2> %s </h2>" % rest.name
+                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'> </form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
+                '''
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -44,15 +92,20 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.headers.getheader('content-type'))
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                messagecontent = fields.get('rname')
+
+            new_restaurant = Restaurant(name=messagecontent[0])
+            session.add(new_restaurant)
+            session.commit()
+
             output = ""
             output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            output += "<h1> Okay, %s has been added </h1>" % messagecontent[0]
+            output += "<a href='/restaurants'>Return to Home Page</a>"
             output += "</body></html>"
             self.wfile.write(output)
             print output
+
         except:
             pass
 
