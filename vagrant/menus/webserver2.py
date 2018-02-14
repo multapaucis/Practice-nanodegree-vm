ba36b6422fca641d5fcadb75ae540d2641a49c70
multapaucis <matthewrfree@gmail.com>
2018-02-14 12:45:04 -0800

    Add Delete Funcionality to WebServer2

--- vagrant/menus/webserver2.py
+++ vagrant/menus/webserver2.py
@@ -26,7 +26,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                 for rest in restaurants:
                     output += "<h2> %s </h2>" % rest.name
                     output += "<a href='/restaurants/%s/edit'>Edit</a><br>" % rest.id
-                    output += "<a href='/edit'>Delete</a><br>"
+                    output += "<a href='/restaurants/%s/delete'>Delete</a><br>" % rest.id
 
                 output += "<br><h2><a href='/restaurants/new'>Add a New Restaurant</a></h2>"
                 output += "</body></html>"
@@ -50,13 +50,12 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 rest_id = self.path[13:-5]
-                print rest_id
 
                 output = ""
                 output += "<html><body>"
                 output += "<form method='POST' enctype='multipart/form-data' "
                 output += "action='/restaurants/edit'>"
-                output += "<h2>What is the restaurant's New Name?</h2>"
+                output += "<h2>What is the New Name?</h2>"
                 output += "<input name='uname' type='text' >"
                 output += "<input type='hidden' name='id' value='%s'>" % rest_id
                 output += "<input type='submit' value='Submit'> </form>"
@@ -64,24 +63,26 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
 
-            else:
-                self.send_error(404, "I can't find %s. try harder" % self.path)
-
-                '''
+            elif self.path.endswith("/delete"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                restaurants = session.query(Restaurant).all()
+                rest_id = self.path[13:-7]
+
                 output = ""
                 output += "<html><body>"
-                for rest in restaurants:
-                    output += "<h2> %s </h2>" % rest.name
-                output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text' ><input type='submit' value='Submit'> </form>"
+                output += "<form method='POST' enctype='multipart/form-data' "
+                output += "action='/restaurants/delete'>"
+                output += "<h2>Delete This Restaurant?</h2>"
+                output += "<input name='delete_rest' type='checkbox' >"
+                output += "<input type='hidden' name='id' value='%s'>" % rest_id
+                output += "<input type='submit' value='Submit'> </form>"
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
                 return
-                '''
+
+            else:
+                self.send_error(404, "I can't find %s." % self.path)
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -131,17 +132,33 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<a href='/restaurants'>Return to Home Page</a>"
                 output += "</body></html>"
                 self.wfile.write(output)
-            '''
-            if new_name:
-                new_restaurant = Restaurant(name=new_name[0])
-                session.add(new_restaurant)
-            if up_name:
-                u_rest =session.query(Restaurant).filter_by(name="")
-                u_rest = Restaurant(name=up_name[0])
-                session.add(new_restaurant)
 
-            session.commit()
-            '''
+            if self.path.endswith("/delete"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    d_rest = fields.get('delete_rest', [None])
+                    rest_id = fields.get('id', None)
+
+                rest = session.query(Restaurant).filter_by(id=rest_id[0]).one()
+                old_name = rest.name
+                output = ""
+                output += "<html><body>"
+                if d_rest[0] == 'on':
+                    session.delete(rest)
+                    session.commit()
+                    output += "<h1> Okay, %s " % old_name
+                    output += "has been removed from the database </h1>"
+                    output += "<a href='/restaurants'>Return to Home Page</a>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+                else:
+                    output += "<h1> Okay, %s " % old_name
+                    output += "will stay in the Database. For now... </h1>"
+                    output += "<a href='/restaurants'>Return to Home Page</a>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
 
         except:
             self.send_error(500, "Something's gone terribly wrong")
